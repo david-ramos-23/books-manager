8aad376e598b7c6d8f80a2a5a8b609c5eac27b6e
David Ramos Ruiz <dramos@squareup.com>
2023-06-12 18:24:50 +0200

    build: :construction_worker: add Dockerfile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+
 function App() {
+
+  useEffect(() => {
+    fetch('/api/users/login', {
+      method: 'POST',
+    }).then((res) => res.json()).then((data) => {
+      console.log(data)
+    })  
+  }, [])
+  
 	return <div>App</div>
 }
 
